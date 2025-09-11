c7e2eece6ffaad4106b7c15a211cd1a337123dac
loganhuss4317 <loganhuss@gmail.com>
2025-09-11 13:17:48 -0400

    email changes to js file

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-// template_5d9wd2v
-// service_shq3dpm
-// QzXk3fkk7BHKSw3Cc
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    emailjs
+    .sendForm(
+       'service_shq3dpm',
+       'template_5d9wd2v',
+       event.target,
+       'QzXk3fkk7BHKSw3Cc'
+    ).then(() =>  {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible"
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable.  Please contact me directly at loganhuss@gmail.com "
+        )
+    })
+}

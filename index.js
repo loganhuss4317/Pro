b953c7032bf5976b0f39a4a73b7e02d2dfd32595
loganhuss4317 <loganhuss@gmail.com>
2025-09-16 15:00:07 -0400

    js update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function moveBackground(event) {
     for (let i = 0; i < shapes.length; ++i) {
         const isOdd = i  % 2 !==0;
         const oddInteger = isOdd ? -1 : 1;
-        shapes[i].style.transform = 'translate(${x * oddInteger}px, ${y * oddInteger}px'
+        shapes[i].style.transform = `translate(${x * oddInteger}px, ${y * oddInteger}px`
     }
 }
 
